ae689cdd6851f4b91a450ac1d5ae9844800da775
Renato Naper <r.naper@spotpromo.com.br>
2020-11-07 08:37:48 -0300

    Migrando todo o layout para objeto

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(home:
 Scaffold(
-  body: Column(
-    children: [
-      Card(
-        child: ListTile(
-          leading: Icon(Icons.monetization_on),
-          title: Text('100'),
-          subtitle: Text('1000'),
-        )
-      ),
-    ],
-  ),
+  body: ListaTransferencias(),
   appBar: AppBar(
     title:
       Text("Transferencias")
@@ -22,3 +12,46 @@ Scaffold(
     ),
   ),
 ));
+
+
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+      children: [
+       ItemTransferencia(Transferencia(100.0,1000)),
+       ItemTransferencia(Transferencia(200.0,1000)),
+       ItemTransferencia(Transferencia(300.0,1000)),
+      ],
+    );
+  }
+  
+}
+
+class ItemTransferencia extends StatelessWidget{
+
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Card(
+        child: ListTile(
+          leading: Icon(Icons.monetization_on),
+          title: Text(_transferencia.valor.toString()),
+          subtitle: Text(_transferencia.numeroConta.toString()),
+        )
+    );
+  }
+
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
